34637df4f5ac46777438701e96b6828eafc482bb
pi22-a <pi22a555@naver.com>
2022-10-31 11:09:59 +0900

    모의고사 완료

--- src/practice/Prepare.java
+++ src/practice/Prepare.java
@@ -1,21 +1,38 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prepare {
-    public int[] solution(int[] answer){
-        int[] su1 = {1,2,3,4,5};
-        int[] su2 = {2,1,2,3,2,4,2,5};
-        int[] su3 = {3,3,1,1,2,2,4,4,5,5};
+    public int[] solution(int[] answers) {
+        int[] su1 = {1, 2, 3, 4, 5};
+        int[] su2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] su3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         int[] count = new int[3];
 
-        for (int i=0; i<answer.length; i++){
-            if(answer[i] == su1[i%5]) count[0]++;
-            if(answer[i] == su2[i%8]) count[1]++;
-            if(answer[i] == su3[i%10]) count[2]++;
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == su1[i % 5]) count[0]++;
+            if (answers[i] == su2[i % 8]) count[1]++;
+            if (answers[i] == su3[i % 10]) count[2]++;
         }
 
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
 
+        List<Integer> list = new ArrayList<>();
 
+        if (max == count[0])
+            list.add(1);
+        if (max == count[1])
+            list.add(2);
+        if (max == count[2])
+            list.add(3);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
 
         return answer;
     }
+}
