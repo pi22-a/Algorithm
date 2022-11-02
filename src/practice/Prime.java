a1c75984990e8cd346eef2760ad89979ea5e2207
pi22-a <pi22a555@naver.com>
2022-11-02 10:06:59 +0900

    소수찾기 에라토스테네세의 체 적용하여 풀이

--- src/practice/Prime.java
+++ src/practice/Prime.java
@@ -1,8 +1,22 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Prime {
+    public int solution(int n) {
+        int anwser = 0;
+        boolean[] arr = new boolean[n + 1];
 
+        for (int i = 2; i * i <= n; i++) {
+            if (!arr[i]) {
+                for (int j = i + i; j <= n; j += i) arr[j] = true;
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!arr[i]) anwser++;
+        }
+        return anwser;
+    }
 }
