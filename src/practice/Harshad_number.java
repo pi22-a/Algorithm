ea6860a39c143231f78ce67a3a5868d3a91448ef
pi22-a <pi22a555@naver.com>
2022-11-07 09:38:23 +0900

    하샤드의 수 완료

--- src/practice/Harshad_number.java
+++ src/practice/Harshad_number.java
@@ -4,5 +4,14 @@ package practice;
 // 12가 3으로 나누어떨어지므로 12는 10진법에서 하샤드 수다.
 // 그러나 16은 1+6=7이고, 16이 7로 나누어떨어지지 않으므로 16은 10진법에서 하샤드 수가 아니다.
 public class Harshad_number {
+    public boolean harshad(int x){
+        int mox = x;
+        int sum = 0;
+        while (x != 0){
+            sum += x % 10;
+            x /= 10;
+        }
+        return  mox % sum == 0 ? true : false;
+    }
 
 }
