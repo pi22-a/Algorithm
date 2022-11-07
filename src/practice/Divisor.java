ddf88269145ce7844aad08939c6aba5004f29ce7
pi22-a <pi22a555@naver.com>
2022-11-07 09:44:54 +0900

    나누어 떨어지는 수 시작

---
+++ src/practice/Divisor.java
@@ -0,0 +1,7 @@
+package practice;
+
+// 배열을 순회하며 divisor로 나누어 떨어지는 값을 list에 저장.
+// list가 빈 경우(divisor로 나누어 떨어지는 element가 하나도 없는 경우) -1을 추가.
+// 값을 오름차순으로 정렬 후 answer배열에 담아 return.
+public class Divisor {
+}
